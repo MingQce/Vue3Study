576ca7962655c240c22dcb10c87c7832cc0a5bde
MingQce <1420395899@qq.com>
2024-05-27 14:09:03 +0800

    App组件 initial

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- html -->
+  <div class="app">
+    <h1>你好,世界</h1>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script lang="ts">
+// JS
+  export default {
+    name:'App'
   }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+/*样式*/
+.app{
+  background-color: #add;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from "vue";  //引入createApp用于创建应用
+import App from './App.vue'  //引入App根组件
 
 createApp(App).mount('#app')
