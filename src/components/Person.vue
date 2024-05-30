75201f1d49330c9dec3db4d86bb8732de3139958
MingQce <1420395899@qq.com>
2024-05-30 11:26:46 +0800

    015 toRefs toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,40 +1,25 @@
 <template>
   <div class="person">
-    <h2>一辆{{car.brand}}车，价值{{car.price}}万</h2>
-    <button @click="changeBrand">修改汽车的品牌</button>
-    <button @click="changePrice">修改汽车的价格</button>
-    <button @click="changeCar">修改汽车</button>
-    <hr>
-    <h2>当前求和为:{{sum}}</h2>
-    <button @click="changeSum">sum+1</button>
+    <h2>姓名:{{person.name}}</h2>
+    <h2>年龄:{{person.age}}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
   </div>
 </template>
 
 <script setup lang="ts" name="Person">  //setup写在这里，会自动return,需要导入插件vite-vue-setup-extend，并在vite.config.js中import,才可以在标签中直接命名组件
-import {reactive, ref} from "vue";  //引入ref
-
-//数据
-let car = reactive({brand: '奔驰', price: 100})  //声明为"对象类型的响应式数据"，ref可以包裹基本数据和对象，reactive只可以包裹对象
-// reactive不可以重新分配一个对象，否则会使其失去响应式数据的特性
-let sum = ref(0)
-
-//方法
-function changePrice(){
-  car.price += 10  //使用ref将对象变为响应式对象时，需要记得先.value，再从value中获取指定数据
+import {reactive, ref, toRefs} from "vue";
+  let person = reactive({  //数据层次较深时推荐用reactive
+    name:'张三',
+    age:18
+  })
+  let{name,age} = toRefs(person)  //解构出name和age，把name和age包裹成新的对象类型响应式数据person
+function changeName(){
+    person.name += "~"
 }
-function changeBrand(){
-  car.brand = '宝马'
+function changeAge(){
+    person.age += 1
 }
-function changeCar(){
-  // reactive
-  Object.assign(car,{brand: '宝马', price: 120})  //想要直接修改整个对象，需要使用assign将后续多个参数的value传到第一个对象参数中去
-  //ref
-  // car.value = {brand: '奥拓',price: 1}
-}
-function changeSum(){
-  sum.value += 1
-}
-
 </script>
 
 <style scoped>
