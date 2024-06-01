70a97ff3fc28449e768e1c0e30e7069cc1ccff2c
MingQce <1420395899@qq.com>
2024-06-01 18:31:16 +0800

    024 ts泛型、接口、自定义类型

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <!-- html -->
-  <h2 ref="title2">你好</h2>
-  <button @click="showLog">点击输出h2</button>
-  <Person ref="ren"/>  <!--vue3允许多个根标签并列-->
+
+  <Person/>  <!--vue3允许多个根标签并列-->
   <!-- 通过ref获取的组件实例，无法查看详细数据 -->
 </template>
 
 <script lang="ts" setup name="App">
   import Person from "@/components/Person.vue";
-  import {ref} from 'vue'
-
-  let title2 = ref()  //app和person中的title2相互独立，类似局部变量
-  let ren = ref()
-  function showLog(){
-    console.log(ren.value)
-  }
 </script>
 
 <style scoped>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="person">
-    <h1>中国</h1>
-    <h2 ref="title2">北京</h2>  <!--  不能用id，因为是全局的，假设App.vue中也有id为title2的元素，会优先使用App.vue中的  -->
-    <h3>东城</h3>
-    <button @click="showLog">点我输出h2这个元素</button>
+    ???
   </div>
 </template>
 
 <script setup lang="ts" name="Person">  //setup写在这里，会自动return,需要导入插件vite-vue-setup-extend，并在vite.config.js中import,才可以在标签中直接命名组件
-  import {ref} from "vue";
-  let title2 = ref()  //创建一个title2用于存储ref标记的内容
-  let a = ref(0)
-  let b = ref(1)
-  let c = ref(2)
+  import {type PersonInter,type Persons} from '@/types'
+  // let person:PersonInter = {id:'aw',name:'张三',age:60}  //定义一个符合person接口规范的person
 
-  function showLog(){
-    console.log(title2.value)
-  }
-  defineExpose({a,b,c})  //声明父类可以调用的数据
+  let personList:Persons = [
+    {id:'1',name:'张三',age:60},
+    {id:'2',name:'李四',age:60},
+    {id:'3',name:'王五',age:60}
+  ]
 </script>
 
 <style scoped>  /*scoped局部样式,将样式限定在当前页面*/
