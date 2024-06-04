b9ca1f3ce7160ef69d188126d9c8ebc1c6d329b2
MingQce <1420395899@qq.com>
2024-06-04 12:06:59 +0800

    031 路由_基本切换效果

--- src/main.js
+++
@@ -1,4 +0,0 @@
-import {createApp} from "vue";  //引入createApp用于创建应用
-import App from './App.vue'  //引入App根组件
-
-createApp(App).mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+//  创建一个路由器，并暴露出去
+
+//1.引入createRouter
+import {createRouter,createWebHistory} from "vue-router";
+//引入可能要呈现的组件
+import Home from '@/components/Home.vue'
+import News from '@/components/News.vue'
+import About from '@/components/About.vue'
+//2.创建路由器
+const router = createRouter({
+    history:createWebHistory(),  //vue3要求：需要制定路由器的工作模式
+    routes:[  //路由规则(一组对应关系,写成对象)
+        {
+            path:'/home',  //路径
+            component:Home  //组件
+        },
+        {
+            path:'/news',  //路径
+            component:News  //组件
+        },
+        {
+            path:'/about',  //路径
+            component:About  //组件
+        }
+    ]
+})
+
+//暴露router
+export default router
