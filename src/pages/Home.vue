dc3f5e0a9f4ae032a405639fa0d2ee509064c70f
MingQce <1420395899@qq.com>
2024-06-06 20:04:43 +0800

    041 路由_编程式路由导航

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script setup lang="ts" name="Home">
+  import {onMounted} from "vue";
+  import {useRouter} from "vue-router";
+
+  const router = useRouter()  //调用路由器,router变为整个路由器的控制者
+
+  // onMounted(()=>{  //页面编程式跳转
+  //   setTimeout(()=>{
+  //     router.push('/news')
+  //   },3000)
+  // })
 
 </script>
 
--- src/pages/News.vue
+++ src/pages/News.vue
@@ -3,7 +3,8 @@
     <!--  导航区  -->
     <ul>
       <li v-for="news in newsList" :key="news.id">
-        <!--  要注意这里是子级路由，需要从父级路径开始写  -->
+        <button @click="showNewsDetail(news)">查看新闻</button>  <!--    在v-for内，可以获取到news参数点击按钮也能查看新闻    -->
+
         <RouterLink
             :to="{
               name:'xinwen_detail',
@@ -27,6 +28,7 @@
 
 <script setup lang="ts" name="News">
   import {reactive} from "vue";
+  import {useRouter} from "vue-router";
 
   const newsList = reactive([
     {id:'01',title:'抗癌食物',content:'西蓝花'},
@@ -34,6 +36,25 @@
     {id:'03',title:'蔬菜',content:'菠菜'},
     {id:'04',title:'球',content:'篮球'}
   ])
+  const router = useRouter()  //获取路由器
+
+  interface NewsInter{  //限制传入的news对象
+    id:string,
+    title:string,
+    content:string
+  }
+
+  function showNewsDetail(news:NewsInter){  //ts：news有个隐式的any类型需要
+    router.push({  //跟to写法一样
+      name:'xinwen_detail',
+      query:{
+        id:news.id,
+        title:news.title,
+        content:news.content
+      }
+    })  //直接通过push跳转
+  }
+
 </script>
 
 <style scoped>
