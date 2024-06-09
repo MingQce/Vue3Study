fb579bb159ed80e0758ee6a8b7584f91ddf1c03a
MingQce <1420395899@qq.com>
2024-06-09 13:51:04 +0800

    051 store组合式写法

--- src/store/fruits.ts
+++ src/store/fruits.ts
@@ -8,24 +8,46 @@ export interface fruitInter{  //定义一个水果对象
     title:string
 }
 export type fruitsInter = fruitInter[]  //定义fruits为一个装有多个水果对象的数组
-export const useFruitsStore = defineStore('fruits',{  //id与文件名相同使用计数相关store
-    actions:{
-        getAFruit(Fruits_basket){
-            //获取对象
-            let obj = Fruits_basket.pop()
-            //包装为fruit
-            if(obj){  //判断是否还有剩余的水果对象
-                let fruit = {id:nanoid(),title:obj.title}
-                this.fruitsList.unshift(fruit)  //后来的放前面
-            }else{
-                console.log("没有水果了")
-            }
-        }
-    },
-    //配置
-    state(){  //真正存储数据的地方,使用reactive包装
-        return{
-            fruitsList:JSON.parse(localStorage.getItem('fruits') as string) || []  //使用parse转化回去。as string断言 || []防止第一次使用时没有数据
+// 选项式写法
+// export const useFruitsStore = defineStore('fruits',{  //id与文件名相同使用计数相关store
+//     actions:{
+//         getAFruit(Fruits_basket){
+//             //获取对象
+//             let obj = Fruits_basket.pop()
+//             //包装为fruit
+//             if(obj){  //判断是否还有剩余的水果对象
+//                 let fruit = {id:nanoid(),title:obj.title}
+//                 this.fruitsList.unshift(fruit)  //后来的放前面
+//             }else{
+//                 console.log("没有水果了")
+//             }
+//         }
+//     },
+//     //配置
+//     state(){  //真正存储数据的地方,使用reactive包装
+//         return{
+//             fruitsList:JSON.parse(localStorage.getItem('fruits') as string) || []  //使用parse转化回去。as string断言 || []防止第一次使用时没有数据
+//         }
+//     }
+// })
+
+import {reactive} from "vue";
+export const useFruitsStore = defineStore('fruits',()=>{  //组合式
+    //相当于state,数据
+    const fruitsList = reactive(
+    JSON.parse(localStorage.getItem('fruits') as string) || []
+    )
+    //相当于actions,方法
+    function getAFruit(Fruits_basket){
+        //获取对象
+        let obj = Fruits_basket.pop()
+        //包装为fruit
+        if(obj){  //判断是否还有剩余的水果对象
+            let fruit = {id:nanoid(),title:obj.title}
+            fruitsList.unshift(fruit)  //后来的放前面
+        }else{
+            console.log("没有水果了")
         }
     }
+    return {fruitsList,getAFruit}  //要交出内容
 })
