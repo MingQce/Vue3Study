0816b53dd039d91f97917a6cb3130e0a2da57525
MingQce <1420395899@qq.com>
2024-06-11 14:29:35 +0800

    063 插槽_作用域插槽

--- src/pages/09/Father.vue
+++ src/pages/09/Father.vue
@@ -2,48 +2,52 @@
   <div class="father">
     <h3>父组件</h3>
     <div class="content">
-      <Category title="游戏列表">
-        <!--   内容不会直接显示，需要插槽     -->
-        <ul>
-          <li v-for="g in games" :key="g.id">{{ g.name }}</li>
-        </ul>
-      </Category>
-      <Category title="美食推荐">
-        <!--    图片    -->
-        <img :src="imageUrl" alt="">
-      </Category>
-      <Category title="热门景点">
-        <!--    视频    -->
-        <video :src="videoUrl" controls></video>
-      </Category>
+      <Game>
+        <!--    params就是子传递过来的数据对象(所有的props)    -->
+        <template v-slot="params">
+          <ul>
+            <li v-for="y in params.youxi" :key="y.id">
+              {{ y.name }}
+            </li>
+          </ul>
+        </template>
+      </Game>
+
+      <Game>
+        <template v-slot="params">
+          <ol>
+            <li v-for="item in params.youxi" :key="item.id">
+              {{ item.name }}
+            </li>
+          </ol>
+        </template>
+      </Game>
+
+      <Game>
+        <template #default="{youxi}">
+          <h3 v-for="g in youxi" :key="g.id">{{ g.name }}</h3>
+        </template>
+      </Game>
+
     </div>
   </div>
 </template>
 
 <script setup lang="ts" name="Father">
-  import Category from "@/pages/09/Category.vue";
-  import {reactive, ref} from "vue";
-  let games = reactive([
-    {id:'01',name:'英雄联盟'},
-    {id:'02',name:'王者农药'},
-    {id:'03',name:'红色警戒'},
-    {id:'04',name:'斗罗大陆'}
-  ])
-  let imageUrl = ref('https://z1.ax1x.com/2023/11/19/piNxLo4.jgp')
-  let videoUrl = ref('')
+  import Game from './Game.vue'
 </script>
 
 <style scoped>
-.father {
-  background-color: rgb(165, 164, 164);
-  padding: 20px;
-  border-radius: 10px;
-}
-.content {
-  display: flex;  /*横向排开*/
-  justify-content: space-evenly;    /*主轴对齐方式*/
-}
-img,video {
-  width: 100%;
-}
+  .father {
+    background-color: rgb(165, 164, 164);
+    padding: 20px;
+    border-radius: 10px;
+  }
+  .content {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  img,video {
+    width: 100%;
+  }
 </style>
